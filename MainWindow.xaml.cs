bdf2f005b8e7daf87ea6335b1d95a4ff10f817b7
Lloyd Pickering <lloyd@lloydpickering.com>
2015-10-29 09:04:30 +0000

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.ComponentModel;
+using Xbim.ModelGeometry.Scene;
+
+namespace XbimFederationChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.SelectedPath = textBox.Text;
+            dlg.ShowNewFolderButton = true;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            var dir = textBox.Text;
+            if (!Directory.Exists(dir))
+            {
+                MessageBox.Show("Please choose a Directory that exists before clicking Start");
+                return;
+            }
+
+            var worker = new BackgroundWorker {
+                WorkerReportsProgress = true
+            };
+            worker.ProgressChanged += delegate (object s, ProgressChangedEventArgs args)
+            {
+                listBox.Items.Add(String.Format("{0}% {1}", args.ProgressPercentage, (string)args.UserState));
+                listBox.ScrollIntoView(listBox.Items[listBox.Items.Count - 1]); 
+            };
+            worker.RunWorkerCompleted += delegate (object s, RunWorkerCompletedEventArgs args)
+            {
+                var Results = args.Result as List<Result>;
+                //MessageBox.Show("Found "+Results.Count+" results");
+                var win = new ResultWindow(Results);
+                win.Show();
+            };
+
+            worker.DoWork += Process;
+            worker.RunWorkerAsync(dir);
+            
+        }
+        void Process(object w, DoWorkEventArgs args)
+        {
+            var worker = w as BackgroundWorker;
+            var dir = args.Argument as string;
+            var dirInfo = new DirectoryInfo(dir);
+
+            var Results = new List<Result>();
+
+            foreach (var f in dirInfo.EnumerateFiles("*.*").Where(s => s.Extension == ".ifc" || s.Extension == ".ifczip"))
+            {
+                worker.ReportProgress(0, String.Format("Starting Processing of {0}", f.Name));
+                using (var m = new Xbim.IO.XbimModel())
+                {
+                    m.CreateFrom(f.FullName, System.IO.Path.ChangeExtension(f.FullName, ".xbim"), worker.ReportProgress, true, true);
+                    var m3d = new Xbim.ModelGeometry.Scene.Xbim3DModelContext(m);
+                    m3d.CreateContext(XbimGeometry.Interfaces.XbimGeometryType.PolyhedronBinary, worker.ReportProgress);
+                    var region = m3d.GetLargestRegion();
+
+                    //string json = JsonConvert.SerializeObject(region, Formatting.Indented);
+                    //File.WriteAllText(args[2], json);
+                    Results.Add(new Result(f.Name, region));
+                }
+                worker.ReportProgress(0, String.Format("Completed Processing of {0}", f.Name));
+            }
+            worker.ReportProgress(100, "Processing Complete.");
+
+            args.Result = Results;
+        }
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XbimFederationChecker
+{
+    public class Result
+    {
+        public Result(String name, Xbim.ModelGeometry.Scene.XbimRegion region)
+        {
+            ModelName = name;
+            WorldCoordinateOffsetX = region.WorldCoordinateSystem.OffsetX;
+            WorldCoordinateOffsetY = region.WorldCoordinateSystem.OffsetY;
+            WorldCoordinateOffsetZ = region.WorldCoordinateSystem.OffsetZ;
+            ModelSizeX = region.Size.X;
+            ModelSizeY = region.Size.Y;
+            ModelSizeZ = region.Size.Z;
+            CentrePointX = region.Centre.X;
+            CentrePointY = region.Centre.Y;
+            CentrePointZ = region.Centre.Z;
+        }
+
+        public String ModelName { get; set; }
+        public Double WorldCoordinateOffsetX { get; set; }
+        public Double WorldCoordinateOffsetY { get; set; }
+        public Double WorldCoordinateOffsetZ { get; set; }
+        public Double ModelSizeX { get; set; }
+        public Double ModelSizeY { get; set; }
+        public Double ModelSizeZ { get; set; }
+
+        public Double CentrePointX { get; set; }
+        public Double CentrePointY { get; set; }
+        public Double CentrePointZ { get; set; }
+    }
+}
